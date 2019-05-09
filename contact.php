bcae86ffb87b6ff4b89b1403e01db84aafcddf4f
dillpill321 <dillpill321@gmail.com>
2019-05-09 15:45:31 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,10 @@
+<!DOCTYPE=html>
+<html>
+<body>
+
+Hello <?php echo $_POST["name"]: ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Subject: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
